14b587c570d75c6f481f14d35b8772bf104ded49
Ayush Bobale <bobaleayush@gmail.com>
2023-07-31 09:55:45 +0530

    comprehension rendrer done

--- source/client/src/components/CompQue/CompQue.jsx
+++ source/client/src/components/CompQue/CompQue.jsx
@@ -1,8 +1,16 @@
 import "./CompQue.css";
 
-import React from "react";
+import React, { useState } from "react";
 
 const CompQue = ({ compPassage, compQuestions }) => {
+  const [answers, setAnswers] = useState({});
+
+  const handleClick = (idx, value) => {
+    setAnswers({ ...answers, [idx]: value });
+  };
+
+  console.log(answers);
+
   return (
     <div className="comp-que">
       <h3>Comprehension Question</h3>
@@ -14,8 +22,24 @@ const CompQue = ({ compPassage, compQuestions }) => {
               <p className="question">
                 {idx + 1}. {elm?.question}
               </p>
-              {elm?.options?.map((option, idx) => {
-                return <p className="option">{option}</p>;
+              {elm?.options?.map((option, optionIdx) => {
+                return (
+                  <p
+                    className="option"
+                    onClick={() => handleClick(idx, optionIdx)}
+                  >
+                    <span
+                      className={
+                        answers[idx] == optionIdx
+                          ? "option-circle selected"
+                          : "option-circle"
+                      }
+                    >
+                      <span>{"\u00A0"}</span>
+                    </span>
+                    {option}
+                  </p>
+                );
               })}
             </div>
           );
