e1905c3bebcdb0c5aa88dfdde9475ecd476a7afb
Ayush Bobale <bobaleayush@gmail.com>
2023-07-31 17:35:11 +0530

    response recorded

--- source/client/src/pages/Responses/NewResponses.jsx
+++ source/client/src/pages/Responses/NewResponses.jsx
@@ -1,10 +1,10 @@
 import "./Response.css";
 
+import { Link } from "react-router-dom";
+import { ROUTES } from "../../config";
 import React from "react";
 import { useGetFormResponsesNewQuery } from "../../redux/slices/formApi";
 import { useParams } from "react-router";
-
-const ResponeRender = () => {};
 
 const NewResponses = () => {
   const { id } = useParams();
@@ -14,6 +14,15 @@ const NewResponses = () => {
     <div className="response-wrap">
       <>
         <h2>{data?.data?.form?.formName}</h2>
+        <div className="responses-to-full">
+          {data?.data?.responses?.map((elm, idx) => {
+            return (
+              <Link to={ROUTES.RESPONSE_FULL(elm?._id)}>
+                View complete Response for {elm?._id}
+              </Link>
+            );
+          })}
+        </div>
       </>
     </div>
   );
